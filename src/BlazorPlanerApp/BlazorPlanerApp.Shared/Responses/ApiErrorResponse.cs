5005062f8ba3578fd2775cf40264b58967e5d0aa
Pétur Siguðsson <gpetur@gmail.com>
2022-04-10 20:08:29 +0000

    Second committ

---
+++ src/BlazorPlanerApp/BlazorPlanerApp.Shared/Responses/ApiErrorResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorPlanerApp.Shared.Responses
+{
+    public class ApiErrorResponse
+    {
+        public string Message { get; set; }
+        public string[] Errors { get; set; }
+        public bool IsSuccess { get; set; }
+    }
+
+}
